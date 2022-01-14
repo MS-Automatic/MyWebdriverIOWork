af018f272d147e7905ac62d9a3e474efcdfe1b8b
MS-Automatic <87861994+MS-Automatic@users.noreply.github.com>
2022-01-14 16:17:24 +0530

    Update ci-pro.test.js
    CI Automation code

--- webdriverio-test/test/specs/ci-pro.test.js
+++ webdriverio-test/test/specs/ci-pro.test.js
@@ -4,6 +4,13 @@ describe("CIService", () => {
       browser.url("file:///D:/JavaScript%20Practice/Practice3.html");
       browser.pause(5000); 
       
+      $("#myButton").waitForExist();
+      $("#myButton").click();
+
+      const txtbox = $("#result");
+      txtbox.waitForExist();
+      
+      expect(txtbox).toHaveValue("9");
     });
 
   });
